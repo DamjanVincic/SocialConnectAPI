80f18054908ed7b788493b4f32941a4efd28a39f
DamjanVincic <damjanvincic2003@gmail.com>
2023-08-30 11:51:20 +0200

    Added DatabaseContext

---
+++ DataAccess/DatabaseContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SocialConnectAPI.Models;
+
+namespace SocialConnectAPI.DataAccess {
+    public class DatabaseContext : DbContext {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                      .HasIndex(u => u.Email)
+                      .IsUnique();            
+        }
+    }
+}
