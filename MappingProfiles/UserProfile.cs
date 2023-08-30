0d3568f69577f77ea50dbe09f91f0fa17fdacb26
DamjanVincic <damjanvincic2003@gmail.com>
2023-08-30 15:14:10 +0200

    Started mapping profiles

---
+++ MappingProfiles/UserProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using DTOs.Users;
+using SocialConnectAPI.DTOs.Users;
+using SocialConnectAPI.Models;
+
+namespace SocialConnectAPI.MappingProfiles {
+    public class UserProfile : Profile {
+        public UserProfile() {
+            CreateMap<User, GetUserResponse>();
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<User, CreateUserResponse>();
+        }
+    }
+}
