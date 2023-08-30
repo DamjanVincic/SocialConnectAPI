bf22eac61f9c7df8dbc68d2b1e37cd334f482ca2
DamjanVincic <damjanvincic2003@gmail.com>
2023-08-30 12:27:15 +0200

    Started creating DTOs

---
+++ DTOs/Users/Get/Response/GetUserResponse.cs
@@ -0,0 +1,13 @@
+using SocialConnectAPI.Models;
+
+namespace DTOs.Users {
+    public class GetUserResponse {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public UserStatus Status { get; set; }
+        public List<User> Following { get; set; }
+        public List<User> Followers { get; set; }
+        public List<Post> LikedPosts { get; set; }
+    }
+}
---
+++ DTOs/Users/Post/Response/UpdateUserResponse.cs
@@ -0,0 +1,8 @@
+namespace SocialConnectAPI.DTOs.Users {
+    public class UpdateUserResponse {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
