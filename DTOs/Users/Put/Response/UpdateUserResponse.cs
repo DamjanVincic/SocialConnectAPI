a79dab2be6b77ff502e76f68b7bc01586d8825f9
DamjanVincic <damjanvincic2003@gmail.com>
2023-08-30 19:56:46 +0200

    Added DTOs for user updating

---
+++ DTOs/Users/Put/Response/UpdateUserResponse.cs
@@ -0,0 +1,15 @@
+using SocialConnectAPI.Models;
+
+namespace SocialConnectAPI.DTOs.Users {
+    public class UpdateUserResponse {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserStatus Status { get; set; }
+        public List<User> Following { get; set; }
+        public List<User> Followers { get; set; }
+        public List<Post> LikedPosts { get; set; }
+    }
+}
