17ee79c529de090bbb0a88cf4a8f75526e65097a
DamjanVincic <damjanvincic2003@gmail.com>
2023-08-30 10:55:28 +0200

    Added Comment model

---
+++ Models/Comment.cs
@@ -0,0 +1,11 @@
+namespace SocialConnectAPI.Models {
+    public class Comment {
+        public int Id { get; set;}
+        // public int UserId { get; set;}
+        public User User { get; set;}
+        // public int PostId { get; set;}
+        public Post Post { get; set;}
+        public string Text { get; set;}
+        public CommentStatus Status { get; set;}
+    }
+}
